6b72a616fbdcbc2a51ff41b6a541a0903764e3ca
hairyf <wwu710632@gmail.com>
2025-09-04 14:11:47 +0800

    chore: update config and fix mapper page

--- nextjs/nextjs-routes.d.ts
+++ nextjs/nextjs-routes.d.ts
@@ -5,11 +5,6 @@
 
 // prettier-ignore
 declare module "nextjs-routes" {
-  import type {
-    GetServerSidePropsContext as NextGetServerSidePropsContext,
-    GetServerSidePropsResult as NextGetServerSidePropsResult
-  } from "nextjs";
-
   export type Route =
     | StaticRoute<"/404">
     | StaticRoute<"/account/api-key">
@@ -32,22 +27,22 @@ declare module "nextjs-routes" {
     | StaticRoute<"/api/monitoring/invalid-api-schema">
     | StaticRoute<"/api/proxy">
     | StaticRoute<"/api-docs">
-    | DynamicRoute<"/apps/[id]", { "id": string }>
     | StaticRoute<"/apps">
+    | DynamicRoute<"/apps/[id]", { "id": string }>
     | StaticRoute<"/auth/profile">
-    | DynamicRoute<"/batches/[number]", { "number": string }>
     | DynamicRoute<"/batches/celestia/[height]/[commitment]", { "height": string; "commitment": string }>
     | StaticRoute<"/batches">
+    | DynamicRoute<"/batches/[number]", { "number": string }>
     | DynamicRoute<"/blobs/[hash]", { "hash": string }>
-    | DynamicRoute<"/block/[height_or_hash]", { "height_or_hash": string }>
-    | DynamicRoute<"/block/countdown/[height]", { "height": string }>
     | StaticRoute<"/block/countdown">
+    | DynamicRoute<"/block/countdown/[height]", { "height": string }>
+    | DynamicRoute<"/block/[height_or_hash]", { "height_or_hash": string }>
     | StaticRoute<"/blocks">
     | DynamicRoute<"/chain/[chain-slug]/accounts/label/[slug]", { "chain-slug": string; "slug": string }>
     | DynamicRoute<"/chain/[chain-slug]/advanced-filter", { "chain-slug": string }>
-    | DynamicRoute<"/chain/[chain-slug]/block/[height_or_hash]", { "chain-slug": string; "height_or_hash": string }>
-    | DynamicRoute<"/chain/[chain-slug]/block/countdown/[height]", { "chain-slug": string; "height": string }>
     | DynamicRoute<"/chain/[chain-slug]/block/countdown", { "chain-slug": string }>
+    | DynamicRoute<"/chain/[chain-slug]/block/countdown/[height]", { "chain-slug": string; "height": string }>
+    | DynamicRoute<"/chain/[chain-slug]/block/[height_or_hash]", { "chain-slug": string; "height_or_hash": string }>
     | DynamicRoute<"/chain/[chain-slug]/csv-export", { "chain-slug": string }>
     | DynamicRoute<"/chain/[chain-slug]/op/[hash]", { "chain-slug": string; "hash": string }>
     | DynamicRoute<"/chain/[chain-slug]/token/[hash]", { "chain-slug": string; "hash": string }>
@@ -58,8 +53,8 @@ declare module "nextjs-routes" {
     | StaticRoute<"/csv-export">
     | StaticRoute<"/deposits">
     | StaticRoute<"/dispute-games">
-    | DynamicRoute<"/epochs/[number]", { "number": string }>
     | StaticRoute<"/epochs">
+    | DynamicRoute<"/epochs/[number]", { "number": string }>
     | StaticRoute<"/gas-tracker">
     | StaticRoute<"/">
     | StaticRoute<"/internal-txs">
@@ -67,20 +62,20 @@ declare module "nextjs-routes" {
     | StaticRoute<"/login">
     | StaticRoute<"/mapper">
     | StaticRoute<"/mud-worlds">
-    | DynamicRoute<"/name-domains/[name]", { "name": string }>
     | StaticRoute<"/name-domains">
+    | DynamicRoute<"/name-domains/[name]", { "name": string }>
     | DynamicRoute<"/op/[hash]", { "hash": string }>
     | DynamicRoute<"/operation/[id]", { "id": string }>
     | StaticRoute<"/operations">
     | StaticRoute<"/ops">
     | StaticRoute<"/output-roots">
-    | DynamicRoute<"/pools/[hash]", { "hash": string }>
     | StaticRoute<"/pools">
+    | DynamicRoute<"/pools/[hash]", { "hash": string }>
     | StaticRoute<"/public-tags/submit">
     | StaticRoute<"/search-results">
     | StaticRoute<"/sprite">
-    | DynamicRoute<"/stats/[id]", { "id": string }>
     | StaticRoute<"/stats">
+    | DynamicRoute<"/stats/[id]", { "id": string }>
     | DynamicRoute<"/token/[hash]", { "hash": string }>
     | DynamicRoute<"/token/[hash]/instance/[id]", { "hash": string; "id": string }>
     | StaticRoute<"/token-transfers">
@@ -89,8 +84,8 @@ declare module "nextjs-routes" {
     | StaticRoute<"/txn-withdrawals">
     | StaticRoute<"/txs">
     | DynamicRoute<"/txs/kettle/[hash]", { "hash": string }>
-    | DynamicRoute<"/validators/[id]", { "id": string }>
     | StaticRoute<"/validators">
+    | DynamicRoute<"/validators/[id]", { "id": string }>
     | StaticRoute<"/verified-contracts">
     | StaticRoute<"/visualize/sol2uml">
     | StaticRoute<"/withdrawals">;
@@ -124,33 +119,6 @@ declare module "nextjs-routes" {
    * route({ pathname: "/foos/[foo]", query: { foo: "bar" }}) will produce "/foos/bar".
    */
   export declare function route(r: Route): string;
-
-  /**
-   * Nearly identical to GetServerSidePropsContext from next, but further narrows
-   * types based on nextjs-route's route data.
-   */
-  export type GetServerSidePropsContext<
-    Pathname extends Route["pathname"] = Route["pathname"],
-    Preview extends NextGetServerSidePropsContext["previewData"] = NextGetServerSidePropsContext["previewData"]
-  > = Omit<NextGetServerSidePropsContext, 'params' | 'query' | 'defaultLocale' | 'locale' | 'locales'> & {
-    params: Extract<Route, { pathname: Pathname }>["query"];
-    query: Query;
-    defaultLocale?: undefined;
-    locale?: Locale;
-    locales?: undefined;
-  };
-
-  /**
-   * Nearly identical to GetServerSideProps from next, but further narrows
-   * types based on nextjs-route's route data.
-   */
-  export type GetServerSideProps<
-    Props extends { [key: string]: any } = { [key: string]: any },
-    Pathname extends Route["pathname"] = Route["pathname"],
-    Preview extends NextGetServerSideProps["previewData"] = NextGetServerSideProps["previewData"]
-  > = (
-    context: GetServerSidePropsContext<Pathname, Preview>
-  ) => Promise<NextGetServerSidePropsResult<Props>>
 }
 
 // prettier-ignore
@@ -165,12 +133,13 @@ declare module "next/link" {
   } from "react";
   export * from "next/dist/client/link";
 
+  type Query = { query?: { [key: string]: string | string[] | undefined } };
   type StaticRoute = Exclude<Route, { query: any }>["pathname"];
 
   export interface LinkProps
     extends Omit<NextLinkProps, "href" | "locale">,
       AnchorHTMLAttributes<HTMLAnchorElement> {
-    href: Route | StaticRoute | Omit<Route, "pathname">
+    href: Route | StaticRoute | Query;
     locale?: false;
   }
 
@@ -198,6 +167,7 @@ declare module "next/router" {
 
   type NextTransitionOptions = NonNullable<Parameters<Router["push"]>[2]>;
   type StaticRoute = Exclude<Route, { query: any }>["pathname"];
+  type Query = { query?: { [key: string]: string | string[] | undefined } };
 
   interface TransitionOptions extends Omit<NextTransitionOptions, "locale"> {
     locale?: false;
@@ -219,12 +189,12 @@ declare module "next/router" {
         locale?: Locale;
         locales?: undefined;
         push(
-          url: Route | StaticRoute | Omit<Route, "pathname">,
+          url: Route | StaticRoute | Query,
           as?: string,
           options?: TransitionOptions
         ): Promise<boolean>;
         replace(
-          url: Route | StaticRoute | Omit<Route, "pathname">,
+          url: Route | StaticRoute | Query,
           as?: string,
           options?: TransitionOptions
         ): Promise<boolean>;
